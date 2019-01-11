ff940fe5a3ee17ff232cc0ef98e81d746b21b59f
Denny Tek <tek.denny@gmail.com>
2019-01-10 19:13:03 -0500

    Edit config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Personal Site Demo`,
+    description: `Personal Website built with Gatsby`,
+    author: ``,
     baseUrl: `https://youthful-yonath-7e38ab.netlify.com/`,
   },
   plugins: [
